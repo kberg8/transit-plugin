b38c7f70c2db028cb3ccd3775438df36da167409
Kelsey Berg <kberg@eagles.ewu.edu>
2014-02-11 14:49:48 -0800

    Final Transitions

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).foundation();
+
+$(".box").transition({	
+	perspective: "100px",
+	rotate3d: "1,1,0,180deg"
+
+
+});
+
+
+
+//console.log("'MERICA!!'"); 
+// Will not appear on the page just explains that everything is connected //
